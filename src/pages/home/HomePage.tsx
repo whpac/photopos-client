2e9f4159c203d59a9243b07d73e65f2d2225a217
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-03-18 17:52:39 +0100

    Make place for map and POI list

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,17 @@
+import './HomePage.scss';
 import Map from '../../views/map/Map';
 import PoiList from '../../views/poiList/PoiList';
 
 function HomePage () {
     return (
-        <>
-            <Map />
-            <PoiList />
-        </>
+        <div className="home-page--container">
+            <div className="home-page--map-box">
+                <Map />
+            </div>
+            <div className="home-page--poi-list-box">
+                <PoiList />
+            </div>
+        </div>
     );
 }
 
