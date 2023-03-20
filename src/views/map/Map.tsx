f212f67d6f3bec9a2374d143ab6caaef8549d304
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-03-20 18:54:46 +0100

    Correct map attribution position

--- src/views/map/Map.tsx
+++ src/views/map/Map.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer, ZoomControl, Marker, Popup } from 'react-leaflet'
+import './Map.scss'
+import { MapContainer, TileLayer, ZoomControl, Marker, Popup, AttributionControl } from 'react-leaflet'
 
 function Map(){
     return (
-        <MapContainer center={[52.4, 16.9]} zoom={13} scrollWheelZoom={true} zoomControl={false}>
+        <MapContainer center={[52.4, 16.9]} zoom={13} scrollWheelZoom={true} zoomControl={false} attributionControl={false}>
             <ZoomControl position="topleft" />
+            <AttributionControl position="bottomleft" />
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
