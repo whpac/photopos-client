5104f575a78b5837643a7f29f7dd7a069626cb05
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-03-26 14:06:27 +0200

    Map is now loaded from local storage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,28 +2,19 @@ import './HomePage.scss';
 import Map from '../../views/map/Map';
 import ActionArea from '../../views/actionArea/ActionArea';
 import MapPointsListener from '../../views/map/MapPointsListener';
-import MapAdapter from '../../views/map/MapAdapter';
-import MapPoint from '../../views/map/MapPoint';
+import StorageMapAdapter from '../../dataModel/mapData/StorageMapAdapter';
+import LocalStorageManager from '../../dataModel/storage/LocalStorageManager';
 
 let mapEventListener = new MapPointsListener();
-
-class DummyMapAdapter implements MapAdapter {
-
-    async getPoints(latMin: number, latMax: number, lngMin: number, lngMax: number): Promise<MapPoint[]> {
-        return [
-            new MapPoint(52.4, 16.9),
-            new MapPoint(52.3, 17.2),
-            new MapPoint(52.5, 17.1),
-            new MapPoint(52.6, 16.8),
-        ];
-    }
-}
+let mapAdapter = new StorageMapAdapter(
+    new LocalStorageManager()
+);
 
 function HomePage () {
     return (
         <div className="home-page--container">
             <div className="home-page--map-box">
-                <Map eventListener={mapEventListener} adapter={new DummyMapAdapter()} />
+                <Map eventListener={mapEventListener} adapter={mapAdapter} />
             </div>
             <div className="home-page--action-area">
                 <ActionArea mapListener={mapEventListener} />
