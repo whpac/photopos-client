3cd92eb791e99ed4fd5629071122b530f2f21a23
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-04-15 16:38:32 +0200

    Restyled the close button

--- src/views/dialog/Dialog.tsx
+++ src/views/dialog/Dialog.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 import './Dialog.scss';
 import DialogController from './DialogController';
 
@@ -13,17 +15,19 @@ function Dialog({ dialogId, children }: DialogProps){
     };
 
     return (
-        <aside className="dialog">
+        <dialog open className="dialog" aria-modal="true">
             <div className="dialog--header">
                 <h2 className="dialog--title">Dialog title</h2>
                 <div className="dialog--header-buttons">
-                    <button className="dialog--close-button" onClick={closeDialog}>X</button>
+                    <button onClick={closeDialog} aria-label="Close" title="Close" type="button">
+                        <FontAwesomeIcon icon={icon({name: 'xmark', style: 'solid'})} />
+                    </button>
                 </div>
             </div>
             <div className="dialog--content">
                 {children}
             </div>
-        </aside>
+        </dialog>
     );
 }
 
