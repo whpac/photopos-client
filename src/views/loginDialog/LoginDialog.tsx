2c6da495bf456cf5ae64f54a7676c22397239588
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-04-16 14:49:26 +0200

    Developing the login form appearance

--- src/views/loginDialog/LoginDialog.tsx
+++ src/views/loginDialog/LoginDialog.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import Dialog from '../dialog/Dialog';
+import Button from '../forms/Button';
+import InputBox from '../forms/InputBox';
+import FieldLayout from '../forms/FieldLayout';
 
 interface LoginDialogProps {
     dialogId: string;
 };
 
-function LoginDialog({ dialogId }: LoginDialogProps){
+function LoginDialog({ dialogId }: LoginDialogProps) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <Dialog dialogId={dialogId} title="Log in">
-            Type your username and password here.
+            <FieldLayout label="Username" inputId="login-username">
+                <InputBox value={username} onChange={setUsername} id="login-username" />
+            </FieldLayout>
+            <FieldLayout label="Password" inputId="login-password">
+                <InputBox type="password" value={password} onChange={setPassword} id="login-password" />
+            </FieldLayout>
+            Forgot password?
+            <Button>Register</Button>
+            <Button isPrimary={true}>Log in</Button>
         </Dialog>
     );
 }
