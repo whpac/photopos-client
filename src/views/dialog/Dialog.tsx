4be09e4ade5443fac18adffc200e5e9bcd5c902f
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-04-17 14:19:59 +0200

    Dialog size selection

--- src/views/dialog/Dialog.tsx
+++ src/views/dialog/Dialog.tsx
@@ -8,21 +8,27 @@ interface DialogProps {
     dialogId: string;
     title: string;
     children: React.ReactNode;
+    size?: 'small' | 'medium' | 'large';
 };
 
-function Dialog({ dialogId, title, children }: DialogProps){
+function Dialog({ dialogId, title, children, size }: DialogProps) {
+    size ??= 'medium';
+
     const closeDialog = () => {
         const controller = DialogController.getInstance();
         controller.closeDialog(dialogId);
     };
 
+    let dialogClasses = ['dialog'];
+    dialogClasses.push(`dialog__${size}`);
+
     return (
-        <dialog open className="dialog" aria-modal="true">
+        <dialog open className={dialogClasses.join(' ')} aria-modal="true">
             <div className="dialog--header">
                 <h2 className="dialog--title">{title}</h2>
                 <div className="dialog--header-buttons">
                     <Button onClick={closeDialog}>
-                        <FontAwesomeIcon icon={icon({name: 'xmark', style: 'solid'})} />
+                        <FontAwesomeIcon icon={icon({ name: 'xmark', style: 'solid' })} />
                     </Button>
                 </div>
             </div>
