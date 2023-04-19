c8b94841be9a35761e571ed62b46432d9bedf9ea
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-04-19 18:21:40 +0200

    The login/logout button checks if user is logged in

--- src/views/navbar/Navbar.tsx
+++ src/views/navbar/Navbar.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
 import DialogController from '../dialog/DialogController';
 import Button from '../forms/Button';
 import LoginDialog from '../login/LoginDialog';
 import "./Navbar.scss";
+import SessionManager from '../../dataModel/session/SessionManager';
 
 function Navbar() {
+    const [sessionId, setSessionId] = useState<string | null>(null);
+
+    useEffect(() => {
+        const sessionManager = SessionManager.getInstance();
+        setSessionId(sessionManager.sessionId);
+
+        const listener = () => setSessionId(sessionManager.sessionId);
+        sessionManager.onSessionIdChanged.addListener(listener);
+        return () => sessionManager.onSessionIdChanged.removeListener(listener);
+    });
+
     const displayLoginDialog = () => {
         const dialogController = DialogController.getInstance();
         const loginDialogId = 'loginDialog';
         dialogController.showDialog(loginDialogId, <LoginDialog dialogId={loginDialogId} />);
     };
+
+    const logOut = () => {
+        SessionManager.getInstance().logOut();
+    };
+
+    const isLoggedIn = sessionId !== SessionManager.SESSION_ID_ANON;
 
     return (
         <nav>
@@ -17,7 +36,8 @@ function Navbar() {
             </div>
             <div className="navRight">
                 <ul>
-                    <li><Button onClick={displayLoginDialog}>Log in</Button></li>
+                    {!isLoggedIn && <li><Button onClick={displayLoginDialog}>Log in</Button></li>}
+                    {isLoggedIn && <li><Button onClick={logOut}>Log out</Button></li>}
                 </ul>
             </div>
         </nav>
