3b0b75f93838d4b3ff5c1d683abbc0ba2de99e86
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-06-04 21:11:44 +0200

    Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ async function main() {
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: https://cra.link/PWA
-    serviceWorkerRegistration.unregister();
+    serviceWorkerRegistration.register();
 }
 
 main();
